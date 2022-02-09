c4ed1a8c79798e792fd01275eafd6422f94ae2e2
Wen Xiao <wennstyle@gmail.com>
2022-02-08 23:51:42 -0500

    commit tree 2b1021697c7bbe432b8fe6b694d9a25f25a2899d

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,10 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteClick, onAnswerChange }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  //console.log(id, prompt, answers, correctIndex) //what gets displayed is the first id,
+  //first prompt, first answers, correctIndex all at once. And then the second id, second etc...
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +12,25 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  //must create a function to house the prop (of another function). The on click does not
+  //'read' a prop. The on event must be assigned to a function, not a reference of it
+    function handleDeleteClick(){
+      onDeleteClick(id);
+    }
+
+    function handleChange(e){
+      onAnswerChange(id, e.target.value)
+    }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]) //initial state should match the type of data
+  //it should hold (otherwise error) which is an array
+
+  useEffect(() =>{
+    fetch("http://localhost:4000/questions")
+    .then((resp) => resp.json())
+    .then((data)=> setQuestions(data))
+  },[])
+
+  function handleDeleteClick(id){
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+    .then((resp) => resp.json())
+    .then(() => {
+      const updatedQuestions = questions.filter((q) => q.id !== id); //filter method for fetch
+      //follows where the state is. If the state was in QuestionItem component, this would be
+      //filtered in the handleDeleteClick function
+      setQuestions(updatedQuestions)
+    });
+  }
+
+  function handleAnswerChange(id, correctIndex){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({correctIndex}),
+    })
+    .then((resp) => resp.json())
+    .then((updatedQuestion) => { //grabbed the json due to it being needed for the map method to find the matching id
+      const updatedAnswer = questions.map((q) => {
+        if(q.id === updatedQuestion.id) return updatedQuestion
+        return q;
+      })
+
+      setQuestions(updatedAnswer)
+
+    })
+  }
+
+  const displayQuestion = questions.map((eachQuestion) => (
+    <QuestionItem
+      key={eachQuestion.id}
+      question={eachQuestion}
+      onDeleteClick={handleDeleteClick}
+      onAnswerChange={handleAnswerChange}
+      /> ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{displayQuestion}</ul>
     </section>
   );
 }
